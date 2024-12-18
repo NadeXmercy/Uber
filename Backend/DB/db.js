f6b0f957f8bfe3fcaf4d77a3b6682d3b0991bad6
Ritik Bhadauriya <7017518052r@gmai.com>
2024-12-18 19:39:29 +0530

    user registration done

---
+++ Backend/DB/db.js
@@ -0,0 +1,15 @@
+const e = require('cors');
+const mongoose = require('mongoose')
+
+
+function connectdb(){
+    
+    mongoose.connect(process.env.DB_CONNECT).then(() => {
+        console.log('db connect succesfully');
+    }).catch(err=>console.log(err));
+    
+   
+    
+}
+
+module.exports = connectdb;
--- Backend/app.js
+++ Backend/app.js
@@ -2,7 +2,10 @@ const dotenv = require("dotenv");
 dotenv.config();
 const cors = require("cors");
 const express = require("express");
+const connectdb = require("./DB/db");
 const app = express();
+const connectTodb = require('./DB/db')
+connectTodb();
 
 app.use(cors());
 app.get("/", (req, res) => {
